18a5472bd2fc6beefba061b174cf38032ed4e129
lm <leo.mangutov@gmail.com>
2018-10-26 14:49:51 -0700

    add for loops to get results for beep, boop

--- js/scripts.js
+++ js/scripts.js
@@ -1,20 +1,29 @@
 function beepBoopFunction(number) {
-  if (number) {
-    var newArray = [...Array(number + 1).keys()];
-    var stringArray = newArray.toString()
-    console.log(stringArray);
-    var splitArray = stringArray.split('');
-    console.log(splitArray);
-    return splitArray
+  if (number >= 4000) {
+    var tooBig = ("that number is too big for Roman Numeral counting");
+    return tooBig;
   }
+  else if (number <= 9 && number >= 0) {
+    var inputArray = number.split('');
+    for (var i = 0; i < (arrayBeepBoopOnes.length+1); i++) {
+      if (parseInt(inputArray[0]) === i ) {
+        var resultArrayPos4 = arrayBeepBoopOnes[i];
+          }
+      // else if (inputArray[0] === "0") {
+      //   var resultArrayPos4 = "you entered zero!";
+      }
+    }
+  }
+    return resultArrayPos4
 }
 
-var arrayBeepBoop = ["Beep!", "Boop!", "I'm sorry, Dave. I'm afraid I can't do that."]
+var arrayBeepBoopOnes = ["Beep!", "Boop!", "", "I'm sorry, Dave. I'm afraid I can't do that.", "", "", "", "", "", ""];
+var arrayBeepBoopTens = [];
 
 $(document).ready(function() {
   $("form#beepboop").submit(function(event) {
   event.preventDefault();
-  var number = parseInt($("input#inputNumber").val());
+  var number = $("input#inputNumber").val();
   var result = beepBoopFunction(number);
 
   $("#result").show();

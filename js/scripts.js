657ba5a14a0fdf9d3ab3f07feb24df108357b3d2
lm <leo.mangutov@gmail.com>
2018-10-26 11:24:23 -0700

    add if else statment to test it

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,19 @@
+function beepBoopFunction(number) {
+  if (number / 3) {
+    var divNum = "I'm sorry, Dave. I'm afraid I can't do that."
+    return divNum;
+  } else (number > 10) {
+    var newNum = "Enter a lower number"
+    return newNum
+  }
+}
+
 $(document).ready(function() {
   $("form#beepboop").submit(function(event) {
   event.preventDefault();
+  var number = parseInt($("input#inputNumber").val());
+  var result = beepBoopFunction(number);
 
-
+  $("#result").show();
+  $("#result").text(result);
 });

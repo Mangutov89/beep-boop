763320c1b37dc8a51bca8fb2fc655c4ab1008daf
lm <leo.mangutov@gmail.com>
2018-10-26 15:23:11 -0700

    made the code work 1 way, going to try another way now

--- js/scripts.js
+++ js/scripts.js
@@ -1,17 +1,17 @@
 function beepBoopFunction(number) {
   if (number >= 4000) {
-    var tooBig = ("that number is too big for Roman Numeral counting");
-    return tooBig;
+    var numberHuge = ("That number is too big!");
+    return numberHuge;
   }
   else if (number <= 9 && number >= 0) {
     var inputArray = number.split('');
-    for (var i = 0; i < (arrayBeepBoopOnes.length+1); i++) {
+    for (var i = 0; i < (arrayBeepBoopOnes.length); i++) {
       if (parseInt(inputArray[0]) === i ) {
         var resultArrayPos4 = arrayBeepBoopOnes[i];
       }
     }
   }
-    return resultArrayPos4
+  return resultArrayPos4
 }
 
 var arrayBeepBoopOnes = ["Beep!", "Boop!", "", "I'm sorry, Dave. I'm afraid I can't do that.", "", "", "I'm sorry, Dave. I'm afraid I can't do that.", "", "", "I'm sorry, Dave. I'm afraid I can't do that."];

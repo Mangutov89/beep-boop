9cf2da3a8b598d8c34f6c440f37d284db90d8772
lm <leo.mangutov@gmail.com>
2018-10-31 20:45:07 -0700

    made the dave message come up with for loop when its divisible by 3

--- js/scripts.js
+++ js/scripts.js
@@ -1,21 +1,14 @@
 function beepBoopFunction(number) {
-  if (number >= 4000) {
-    var numberHuge = ("That number is too big!");
-    return numberHuge;
-  }
-  else if (number <= 9 && number >= 0) {
-    var inputArray = number.split('');
-    for (var i = 0; i < (arrayBeepBoopOnes.length); i++) {
-      if (parseInt(inputArray[0]) === i ) {
-        var resultArrayPos4 = arrayBeepBoopOnes[i];
-      }
+  var emptyArray = [];
+  for (var i = 0; i < number; i++) {
+    if (i % 3 === 0) {
+      emptyArray.push(" I'm sorry Dave , I can't do that");
+      console.log(emptyArray);
     }
   }
-  return resultArrayPos4
-}
+  return emptyArray;
+};
 
-var arrayBeepBoopOnes = ["Beep!", "Boop!", "", "I'm sorry, Dave. I'm afraid I can't do that.", "", "", "I'm sorry, Dave. I'm afraid I can't do that.", "", "", "I'm sorry, Dave. I'm afraid I can't do that."];
-var arrayBeepBoopTens = [];
 
 $(document).ready(function() {
   $("form#beepboop").submit(function(event) {
@@ -23,7 +16,7 @@ $(document).ready(function() {
   var number = $("input#inputNumber").val();
   var result = beepBoopFunction(number);
 
-  $("#result").show();
+  $("#result").show(result);
   $("#result").text(result);
   });
 });

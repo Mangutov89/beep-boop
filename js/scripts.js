084b1de1fede17bc6f813bb9ccfe348a3ba23346
lm <leo.mangutov@gmail.com>
2018-10-31 22:17:52 -0700

    add a name input to replace dave with an inputted name

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,9 @@
 function beepBoopFunction(number) {
   var emptyArray = [];
+  var name = $("#inputName").val();
   for (var i = 0; i <= number; i++) {
     if (i % 3 === 0 && i !== 0) {
-      emptyArray.push(" I'm sorry Dave , I can't do that");
+      emptyArray.push(" I'm sorry " + name + ", I can't do that ");
     } else if (i.toString().split("").includes("1")) {
       emptyArray.push(" Boop!");
     } else if (i.toString().split("").includes("0")) {

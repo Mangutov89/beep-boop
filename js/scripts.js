ec5b3a69e44cf8bc31bfd214dba516fc137124c8
lm <leo.mangutov@gmail.com>
2018-10-31 22:05:39 -0700

    changed the < to <= in my for loop, now it calculates 0

--- js/scripts.js
+++ js/scripts.js
@@ -1,15 +1,12 @@
 function beepBoopFunction(number) {
   var emptyArray = [];
-  for (var i = 0; i < number; i++) {
+  for (var i = 0; i <= number; i++) {
     if (i % 3 === 0 && i !== 0) {
       emptyArray.push(" I'm sorry Dave , I can't do that");
-      console.log(emptyArray);
     } else if (i.toString().split("").includes("1")) {
       emptyArray.push(" Boop!");
-      console.log("working3");
     } else if (i.toString().split("").includes("0")) {
       emptyArray.push(" Beep!");
-      console.log("working4");
     } else {
       emptyArray.push(i);
     }
